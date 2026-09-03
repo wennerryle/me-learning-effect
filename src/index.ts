1b63bfda7a845f26981d8d37c93976d21056ca04
Vladislav Pozdeev <deltawenny@ya.ru>
2026-09-03 03:51:56 +0300

    feat: add catchTag

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,17 @@
-import { Console, Effect } from "effect";
+import { Console, Effect, pipe } from "effect";
 
-const fetchRequest = Effect.promise(() =>
+const fetchRequest = Effect.tryPromise(() =>
     fetch("https://pokeapi.co/api/v2/pokemon/garchomp/")
 );
 
 const jsonResponse = (response: Response) =>
-    Effect.promise(() => response.json());
+    Effect.tryPromise(() => response.json());
 
-const pokemonStats = Effect.flatMap(
+const main = pipe(
     fetchRequest,
-    jsonResponse,
-)
-
-const main = Effect.flatMap(
-    pokemonStats,
-    Console.log
+    Effect.flatMap(jsonResponse),
+    Effect.flatMap(Console.log),
+    Effect.catchTag("UnknownException", () => Effect.succeed("Something bad happened."))
 )
 
 Effect.runPromise(main);
