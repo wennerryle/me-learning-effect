f563ff33cdc021a11bcd265f1e9c0481f4239e29
Vladislav Pozdeev <deltawenny@ya.ru>
2026-09-03 04:32:13 +0300

    feat: introduce Data.TaggedError

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,31 @@
 import { Console, Effect, pipe } from "effect";
+import { FetchError, JsonError } from "./errors";
 
-const fetchRequest = Effect.tryPromise(() =>
-    fetch("https://pokeapi.co/api/v2/pokemon/garchomp/")
-);
+const fetchRequest = Effect.tryPromise({
+    try: () => fetch("https://pokeapi.co/api/v2/pokemon/garchomp124/"),
+    catch: () => new FetchError(),
+}).pipe(Effect.filterOrFail((resp) => resp.ok, () => new FetchError()));
 
 const jsonResponse = (response: Response) =>
-    Effect.tryPromise(() => response.json());
+    Effect.tryPromise({
+        try: () => response.json(),
+        catch: () => new JsonError(),
+    });
 
 const main = pipe(
     fetchRequest,
     Effect.flatMap(jsonResponse),
+    Effect.catchTags({
+        FetchError: () =>
+            Effect.succeed(
+                "Cannot check the API. Please check internet connection",
+            ),
+        JsonError: () =>
+            Effect.succeed(
+                "Cannot parse Garchomp data. Maybe API is outdated.",
+            ),
+    }),
     Effect.flatMap(Console.log),
-    Effect.catchTag("UnknownException", () => Effect.succeed("Something bad happened."))
-)
+);
 
 Effect.runPromise(main);
